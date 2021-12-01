a1df2f2ec51cff226ccf2a39e507e2a30b15670d
triozer <boirardcedric@gmail.com>
2021-12-01 21:19:34 +0100

    day1: add input and test files

--- src/day1/main.kt
+++ src/day1/main.kt
@@ -1,3 +1,7 @@
+package day1
+
+import readInput
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input.size
@@ -8,10 +12,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("day1/test")
+    check(part1(testInput) == 7)
 
-    val input = readInput("Day01")
+    val input = readInput("day1/input")
     println(part1(input))
     println(part2(input))
 }
