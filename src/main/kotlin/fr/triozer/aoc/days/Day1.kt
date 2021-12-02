ba2a26fdfe9b609a36e640f10eb73903dcf26d13
triozer <triozer1@gmail.com>
2021-12-02 16:55:28 +0100

    refactor: remove useless map call

--- src/main/kotlin/fr/triozer/aoc/days/Day1.kt
+++ src/main/kotlin/fr/triozer/aoc/days/Day1.kt
@@ -4,7 +4,7 @@ import fr.triozer.aoc.readInputAsInt
 
 fun part1(input: List<Int>) = input.zipWithNext().count { (a, b) -> b > a }
 
-fun part2(input: List<Int>) = part1(input.windowed(3).map { it.sum() })
+fun part2(input: List<Int>) = part1(input.windowed(3) { it.sum() })
 
 fun main() {
     val testInput = readInputAsInt(1, "test")
